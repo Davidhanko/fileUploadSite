5a7752b65fe48a9082a6c7025119cdfecc250c7f
Davihan11 <davihan11@gmail.com>
2024-08-23 12:35:46 +0200

    Delete prisma/migrations directory

--- prisma/migrations/20240817134554_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Folder" ADD COLUMN     "parentUuid" VARCHAR(255);
-
--- AddForeignKey
-ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentUuid_fkey" FOREIGN KEY ("parentUuid") REFERENCES "Folder"("uuid") ON DELETE SET NULL ON UPDATE CASCADE;
